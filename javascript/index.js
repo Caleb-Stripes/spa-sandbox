91b2510432fb8d452d2c9897653d310d0e2fd195
Caleb Stripes <calebdstripes@gmail.com>
2020-04-30 17:26:07 -0400

    had some fun

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,9 @@
 const image1 = "./resources/Navigator.JPG";
 const img1Lab = "Ship's Navigator"
+const img1BD = "./resources/Deck of the Lady.JPG"
 const image2 = "./resources/Man and Goats.JPG";
 const img2Lab = "Mountaineer"
+const img2BD = "./resources/Lookout Tower.JPG"
 
 
 var currentImage = image1;
@@ -14,11 +16,13 @@ function next() {
         currentImage = image2; 
         document.getElementById('photo').src = image2;
         document.getElementById('photoLabel').innerHTML = img2Lab;
+        document.getElementById('photoBackDrop').src = img2BD;
     } else {
         currentImage = image1;
         document.getElementById('photo').src = image1;
         document.getElementById('photoLabel').innerHTML = img1Lab;
-}
+        document.getElementById('photoBackDrop').src = img1BD;
+    }
 }
 
 
