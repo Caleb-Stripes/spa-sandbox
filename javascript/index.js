85ae1a1cd665df16d11f1d6451decbcb915f6cd2
Caleb Stripes <calebdstripes@gmail.com>
2020-04-30 16:52:36 -0400

    added a photo and description

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,24 @@
+const image1 = "./resources/Navigator.JPG";
+const img1Lab = "Ship's Navigator"
+const image2 = "./resources/Man and Goats.JPG";
+const img2Lab = "Mountaineer"
 
 
-function effect(e) {
-    const button = document.querySelector('#change')
-    button.classList.add('fun')
-    
+var currentImage = image1;
+
+document.querySelector('#change').addEventListener('click', next);
+
+var currentIndex = 1;
+function next() {
+    if (currentImage == image1) {
+        currentImage = image2; 
+        document.getElementById('photo').src = image2;
+        document.getElementById('photoLabel').innerHTML = img2Lab;
+    } else {
+        currentImage = image1;
+        document.getElementById('photo').src = image1;
+        document.getElementById('photoLabel').innerHTML = img1Lab;
 }
+}
+
+
