8d82532ef4ab19213227c787d647cd1811a7fff9
Caleb Stripes <calebdstripes@gmail.com>
2020-05-27 12:04:27 -0400

    added logo to about me

--- javascript/about-me.js
+++ javascript/about-me.js
@@ -8,11 +8,15 @@ const img1BD = "./resources/Deck of the Lady.JPG"
 const image2 = "./resources/Man and Goats.JPG"
 const img2Lab = "Mountaineer"
 const img2BD = "./resources/Lookout Tower.JPG"
+const image3 = "./resources/Logo.jpg"
+const img3Lab = "My Logo"
+const img3BD = "./resources/Blank.JPG"
 
 //  ******** Paragraph Populator ********
 const info0 = "Software Development has always been something I have been interested in. This journey has been unlike any other journey I have had, I am able to bring the same mentality to challenges faced but the solutions and problem solving tactics are completely new. Development is so rewarding, I find it a great outlet for my creativity and logical practice."
 const info1 = "Between 2012 and 2016, I spent nearly all my time crewing aboard Tall Ships. These maritime adventures saw me from coast to coast and from B.C. Canada to San Diego and back. These years came with a plethora of new skills and exciting times, but most notably it defined my abilities to work on a team. There are few environments I have found that forge such bonds. And yeah, I climbed the rigging every day."
 const info2 = "Growing up in Western Washington, the mountains and the sound became daily reminders of the natural beauty in the world. Albeit I didn't truly grow to appreciate them until I left. However, it was not before establishing an attitude of 'Get up and Go!' when it came to adventure." 
+const info3 = "Something I whipped up just for fun."
 
 function setInital() {
         document.getElementById('photo').src = image0;
@@ -44,6 +48,13 @@ function nextImage() {
         document.getElementById('infoText').innerHTML = info2;
     }
     else if (currentImage == image2) {
+        currentImage = image3;
+        document.getElementById('photo').src = image3;
+        document.getElementById('photoLabel').innerHTML = img3Lab;
+        document.getElementById('photoBackDrop').src = img3BD;
+        document.getElementById('infoText').innerHTML = info3;
+    }
+    else if (currentImage == image3) {
         currentImage = image0;
         document.getElementById('photo').src = image0;
         document.getElementById('photoLabel').innerHTML = img0Lab;
